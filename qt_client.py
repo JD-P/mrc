45ff5950f500d57521481956d080371517341134
John David Pressman <jdpostage@linuxmail.org>
2015-09-28 02:05:23 -0700

    Initial 'complete' commit. Not currently working.

--- qt_client.py
+++ qt_client.py
@@ -1,13 +1,13 @@
 from qa_client import *
-from PySide import QtCore
-from PySide import QtGui
+from PySide.QtCore import *
+from PySide.QtGui import *
 import json
 import sys
 
 
-qt_app = QtGui.QApplication(sys.argv)
+qt_app = QApplication(sys.argv)
 
-class QuestionAnswerSystem(QtGui.QWidget):
+class QuestionAnswerSystemClient(QWidget):
     def __init__(self):
         logic = QAClientLogic()
         self.config = self.read_config(logic.confpath)
@@ -15,22 +15,27 @@ class QuestionAnswerSystem(QtGui.QWidget):
         self.setWindowTitle("Makerspace QA System")
         self.setMinimumWidth(600)
         # Create the top level layouts and widgets of the program
-        self.layout = QtGui.QVBoxLayout()
-        self.room_info = QtGui.QHBoxLayout()
-        self.chat_core = QtGui.QHBoxLayout()
-        self.control_panel = QtGui.QHBoxLayout()
-        self.chat_bar = QtGui.QLineEdit(self)
+        self.layout = QVBoxLayout()
+        self.room_info = QHBoxLayout()
+        self.chat_core = QHBoxLayout()
+        self.control_panel = QHBoxLayout()
+        self.chat_bar = QLineEdit(self)
         # Create the room info widgets
-        self.discussion_topic = QtGui.QLabel("Placeholder", self)
-        self.room_address = QtGui.QLabel(self.config["client"]["default_host"], self)
+        self.discussion_topic = QLabel("Placeholder", self)
+        self.room_address = QLabel(self.config["client"]["default_host"], self)
         self.room_info.addWidget(self.discussion_topic)
         self.room_info.addWidget(self.room_address)
         # Create the chat core widgets
-        self.discussion_view_frame = QtGui.QFrame(self)
-        self.discussion_view = QtGui.QTextDocument(self)
-        self.user_list = QtGui.QTextDocument(self)
+        self.discussion_view_frame = QFrame(self)
+        self.discussion_view = QTextDocument(self)
+        self.user_list = QTextDocument(self)
         # Create the control panel widgets
-        self.
+        if os.name == 'posix':
+            iconpath = "icons/"
+        elif platform.system() == 'Windows':
+            iconpath = "icons\\"
+        self.screenshot_icon = QIcon(iconpath + "application_add.png")
+        self.mute_indicator_icon = QIcon(iconpath + "comment.png")
 
     def read_config(self, confpath):
         """Read the configuration file and return a JSON dictionary representing
