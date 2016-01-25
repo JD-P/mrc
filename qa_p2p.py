197793cd5622244ec301f32ed03d9f03afc59647
John David Pressman <jdpostage@linuxmail.org>
2016-01-24 17:03:44 -0800

    Renamed Dialer to P2PNode and fleshed out node architecture
    Added connection_error event attribute to client logic in qa_client.
    
    Wrote P2PNode loop_forever() which goes through one connection recovery cycle when
    the client logic signals an error.
    
    Wrote callback() which implements the portion that determines the new server IP
    address by querying an address book of old ones and asking peers.
    
    When a promising address is found in this way it is verified with the infrastructure
    written with sident_verify(), send_loop(), receive_loop(), etc. receive_loop() also
    handles queries from other clients looking to know our best guess at the server IP.

--- qa_p2p.py
+++ qa_p2p.py
@@ -2,35 +2,35 @@ from Crypto.PublicKey import DSA
 from Crypto.Hash import SHA256
 from Crypto.Random import random
 import socket
+import socketserver
+import threading
+import queue
 import base64
+import json
 
-class QAKey(DSA):
-    """Question answer system keypair system. This is a basic DSA signing key
-    generation class, this class adds methods to dump public key params to base64 
-    and a fingerprint of the key based on such."""
-    def base64_pub_encode(self, key):
-        """Return a base64 representation of the public key. The representation is
-        just the variables y g p q concatenated together with colon seperators
-        and then encoded."""
-        (y, g, p, q) = (str(key.y), str(key.g), str(key.p), str(key.q))
-        return base64.b64encode((y + ":" + g + ":" + p + ":" + q).encode('utf-8')).decode('utf-8')
+def base64_pub_encode(self, key):
+    """Return a base64 representation of the public key. The representation is
+    just the variables y g p q concatenated together with colon seperators
+    and then encoded."""
+    (y, g, p, q) = (str(key.y), str(key.g), str(key.p), str(key.q))
+    return base64.b64encode((y + "," + g + "," + p + "," + q).encode('utf-8')).decode('utf-8')
 
-    def base64_pub_decode(self, base64_pub):
-        """Return a tuple with the variables y g p q given a base64 representation
-        of a DSA public key."""
-        base64_pub_bytes = base64_pub.encode('utf-8')
-        pubkey_text = base64.b64decode(base64_pub_bytes)
-        pubkey_vars = pubkey_text.split(":")
-        y = int(pubkey_vars[0])
-        g = int(pubkey_vars[1])
-        p = int(pubkey_vars[2])
-        q = int(pubkey_vars[3])
-        return self.construct((y,g,p,q))
+def base64_pub_decode(self, base64_pub):
+    """Return a tuple with the variables y g p q given a base64 representation
+    of a DSA public key."""
+    base64_pub_bytes = base64_pub.encode('utf-8')
+    pubkey_text = base64.b64decode(base64_pub_bytes)
+    pubkey_vars = pubkey_text.split(":")
+    y = int(pubkey_vars[0])
+    g = int(pubkey_vars[1])
+    p = int(pubkey_vars[2])
+    q = int(pubkey_vars[3])
+    return DSA.construct((y,g,p,q))
 
-    def fingerprint(self, key):
-        """Return a SHA256 fingerprint of a base64 encoded public key."""
-        base64_pub = self.base64_pub_encode(key)
-        return SHA256.new(base64_pub.encode('utf-8')).digest()
+def fingerprint(self, key):
+    """Return a SHA256 fingerprint of a base64 encoded public key."""
+    base64_pub = self.base64_pub_encode(key)
+    return SHA256.new(base64_pub.encode('utf-8')).digest()
         
 
 class ClientList():
@@ -62,20 +62,20 @@ class ServerAddressBook():
         self.book = dict()
         
     def add_server(self, key):
-        base64_pub = QAKey.base64_pub_encode(key)
+        base64_pub = base64_pub_encode(key)
         if base64_pub not in self.book:
             self.book[base64_pub] = set()
         else:
             return False
 
-    def add_address(self, key, ip_address, timestamp, signature):
+    def add_address(self, key, ip_address, timestamp, signature, port=9665):
         """Add an address to the list that a server has hosted at. Each address
         includes a timestamp of when the server signed it so that it's easy to
         sort and determine what the most recent address is. The dialer goes
         through each entry in this list in the case of a disconnection or failure
         to connect."""
-        base64_pub = QAKey.base64_pub_encode(key)
-        digest = SHA256.new(ip_address + str(timestamp)).digest()
+        base64_pub = base64_pub_encode(key)
+        digest = SHA256.new(ip_address + "," + str(port) + "," + str(timestamp)).digest()
         if not key.verify(digest, signature):
             return False
         if (ip_address, timestamp, signature) not in self.book[base64_pub]:
@@ -84,7 +84,7 @@ class ServerAddressBook():
             return False
 
     def remove_server(self, key):
-        base64_pub = QAKey.base64_pub_encode(key)
+        base64_pub = base64_pub_encode(key)
         if base64_pub in self.book:
             self.book.pop(base64_pub)
             return True
@@ -92,22 +92,32 @@ class ServerAddressBook():
             return False
 
     def list_by_key(self, key):
-        base64_pub = QAKey.base64_pub_encode(key)
+        base64_pub = base64_pub_encode(key)
         if base64_pub in self.book:
             return self.book[base64_pub]
         else:
             return False
 
-class Dialer():
-    """Implements the callback function of the QA p2p system. This class handles 
-    client dialing by default, but the subclass ServerDialer handles the callback
-    function from the server side.
+class P2PNode(socketserver.ThreadingMixIn, socketserver.TCPServer):
+:
+    """Implements the callback function of the QA p2p system. (For a description
+    of this function see the callback() method.) This class handles client dialing 
+    by default, but the subclass ServerNode handles the callback function from 
+    the server side.
     """
-    def __init__(self, address_book, client_list, key):
+    def __init__(self, client_logic, address_book, client_list, key):
+        self._client_logic = client_logic
         self._address_book = address_book
         self._client_list = client_list
         self._key = key
+        self._send_queue = queue.Queue()
+        self._shutdown = threading.Event()
 
+    def loop_forever(self):
+        while True:
+            self._client_logic.connection_error.wait()
+            self.callback()
+        
     def callback(self):
         """
         When a connection is lost to the server, the callback() method goes through the 
@@ -126,11 +136,145 @@ class Dialer():
         for address in server_addresses:
             try:
                 connection = socket.create_connection((address, 9665))
-                self.verify(connection)
+                self.sident_verify(connection)
             except socket.error:
                 continue
             
 
-    def verify(self, connection):
+    def sident_verify(self, connection):
         """Request the server send a signed verification of its identity with 
-        IP address and timestamp."""
+        IP address, port and timestamp.
+
+        sident stands for 'Server Identity'
+
+        An sident_verify message is of the following form:
+
+        {'type':'sident_verify'}
+
+        The server should reply with an sident_response message which is of
+        the following form:
+
+        {'type':'sident_response',
+         'ip_addr':<IP ADDRESS AS A STRING>,
+         'port':<PORT NUMBER AS AN INTEGER>,
+         'timestamp':<UNIX TIMESTAMP>,
+         'signature':<SIGNED DIGEST OF THE THREE PREVIOUS VALUES AS A UTF-8 STRING 
+                      CONCATENATED TOGETHER WITH COMMA SEPERATORS>}
+        """
+        sident_verify_msg = {'type':'sident_verify'}
+        self._send_queue.put(sident_verify_msg)
+        return True
+
+    def send_loop(self):
+        """Send loop that is meant to be started from a seperate thread of 
+        execution. The send loop pulls 'raw' python object messages from this 
+        objects send_queue attribute and converts them to json strings before 
+        encoding them as utf-8 to send across the wire.
+
+        Responses are handled and received by the receive_loop method of this class
+        which is ran in a seperate thread of execution."""
+        while not self._shutdown.is_set():
+            message = self._send_queue.get()
+            message_length = self._calculate_recursive_length(message)
+            wrapped_message = [message_length, message]
+            wire_message = (json.dumps(wrapped_message) + "\r\n\r\n").encode('utf-8')
+            self.connection.sendall(wire_message)
+        return True
+
+    def receive_loop(self):
+        """Receive loop that is meant to be started from a seperate thread of
+        execution. The receive loop takes in 'raw' utf-8 json messages from the
+        wire and decodes them, then interprets them to produce native python 
+        objects. The resulting objects are then handled by a method of this class
+        of the form handle_<message_type>. For example if a message with the 
+        'type' key 'test' came in like so:
+
+        {'type':'test'}
+
+        The method self.handle_test(message) would be called with the message
+        dictionary object passed along.
+        """
+        msg_buffer = bytes() # The message input buffer
+        while not self._shutdown.is_set():
+            if msg_buffer:
+                try:
+                    msg_length = self.determine_length_of_json_msg(msg_buffer)
+                except InvalidLengthHeader:
+                    msg_length = float("inf")
+                if len(msg_buffer) >= msg_length:
+                    message = self.extract_msg(msg_buffer, msg_length)
+                    try:
+                        handler = getattr(self, "handle_" + message['type'])
+                    except AttributeError:
+                        print("Can't handle message of type: " +
+                              str(message['type']))
+                        continue
+                    handler(message)
+                    msg_buffer = msg_buffer[msg_length:]
+                else:
+                    try:
+                        msg_buffer += connection.recv(1024)
+                    except socket.timeout:
+                        pass
+            else:
+                try:
+                    msg_buffer += connection.recv(1024)
+                except socket.timeout:
+                    pass
+
+    def determine_length_of_json_msg(self, message_bytes):
+        """Incrementally parse a JSON message to extract the length header.
+
+        message_bytes: The bytes that represent the portion of the message 
+        recieved.
+        """
+        # All messages must be written in utf-8
+        message = message_bytes.decode('utf-8')
+        # Check that the message we have been given looks like a valid length header
+        if "," not in message:
+            raise InvalidLengthHeader(message)
+        length_portion = message.split(",")[0]
+        left_bracket = length_portion[0] == "["
+        number_before_comma = length_portion[-1] in "1234567890"
+        if left_bracket and number_before_comma:
+            for character in enumerate(length_portion):
+                if character[1] not in "[ \n\t\r1234567890,":
+                    raise InvalidLengthHeader(length_portion)
+                elif character[1] in "1234567890":
+                    length_start = character[0]
+                    return int(length_portion[length_start:])
+        elif left_bracket:
+            raise InvalidLengthHeader(length_portion)
+        else:
+            raise MissingLengthHeader(length_portion)
+        return False
+
+    def extract_msg(self, msg_buffer, length):
+        message = msg_buffer[:length].decode()
+        try:
+            right_curly_bracket = message[-6] == "}" or message[-2] == "}"
+        except IndexError:
+            print(message, msg_buffer, length)
+        valid_delimiter = message[-6:] == "}]\r\n\r\n"
+        if right_curly_bracket and valid_delimiter:
+            return message
+        elif right_curly_bracket:
+            raise InvalidMessageDelimiter(message)
+        else:
+            raise MissingMessageDelimiter(message)
+
+    def _calculate_recursive_length(self, msg_dict):
+        """Calculate the length of a dictionary represented as JSON once a length
+        field has been added as a key."""
+        delimiter = "\r\n\r\n"
+        initial_length = len(
+            json.dumps(msg_dict) + delimiter)
+        initial_list = [initial_length, msg_dict]
+        recursive_length = len(
+            json.dumps(initial_list) + delimiter)
+        recursive_list = [recursive_length, msg_dict]
+        while len(json.dumps(recursive_list) + delimiter) != recursive_list[0]:
+            recursive_length = len(
+                json.dumps(recursive_list) + delimiter)
+            recursive_list = [recursive_length, msg_dict]
+        return recursive_list[0]
