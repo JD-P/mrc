bc1070ec26fdc46c64c8ad75acfd5f17088f2f30
John David Pressman <jdpostage@linuxmail.org>
2016-02-05 14:18:05 -0800

    Added most_recent tracker to ServerAddressBook, wrote request_server_address
    Added timestamps to the sident_verify message.

--- qa_p2p.py
+++ qa_p2p.py
@@ -6,6 +6,8 @@ import socket
 import socketserver
 import threading
 import queue
+import time
+import calendar
 import base64
 import json
 
@@ -51,10 +53,10 @@ class ClientList():
     def __init__(self):
         self._neighbors = set()
 
-    def add(self, address):
+    def add(self, address, port):
         if type(address) != str:
             raise self.IPAddError("Expected IP address to be of type string.")
-        self._neighbors.add(address)
+        self._neighbors.add((address, port))
 
     def list(self):
         return self._neighbors
@@ -97,6 +99,15 @@ class ServerAddressBook():
             return False
         if (ip_address, timestamp, signature) not in self.book[base64_pub]:
             self.book[base64_pub].add((ip_address, timestamp, signature))
+            if 'most_recent' in self.book:
+                if int(self.book[base64_pub][1]) > int(self.book['most_recent'][1]):
+                    self.book['most_recent'] = base64_pub
+                    return True
+                else:
+                    return True
+            else:
+                self.book['most_recent'] = base64_pub
+                return True
         else:
             return False
 
@@ -149,19 +160,23 @@ class P2PNode(socketserver.ThreadingMixIn, socketserver.TCPServer):
         
         If an entry is valid and not already in the book it will be added and tried.
         Finally if this process fails to yield the server address the client will need
-        to be reconfigured manually. Otherwise dial() returns the servers new address.
-        server_addresses = address_book.list_by_key(key)
-        """
+        to be reconfigured manually. Otherwise dial() returns the servers new address."""
+        server_addresses = self._address_book.list_by_key(key)
         for address in server_addresses:
             if self._client_logic.connection_error.is_set():
                 try:
-                    connection = socket.create_connection((address, 9665))
+                    connection = socket.create_connection((address[0], 9665))
                     self.sident_verify(connection, v_event)
                 except socket.error:
                     continue
             else:
                 return True
-            
+        neighbor_addresses = self._client_list.list()
+        for address in neighbor_addresses:
+            if self._client_logic.connection_error.is_set():
+                try:
+                    connection = socket.create_connection((address[0], address[1]))
+                    
 
     def sident_verify(self, connection):
         """Request the server send a signed verification of its identity with 
@@ -171,7 +186,8 @@ class P2PNode(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
         An sident_verify message is of the following form:
 
-        {'type':'sident_verify'}
+        {'type':'sident_verify'
+         'timestamp':<UNIX TIMESTAMP>}
 
         The server should reply with an sident_response message which is of
         the following form:
@@ -181,11 +197,36 @@ class P2PNode(socketserver.ThreadingMixIn, socketserver.TCPServer):
          'port':<PORT NUMBER AS AN INTEGER>,
          'timestamp':<UNIX TIMESTAMP>,
          'signature':<SIGNED DIGEST OF THE THREE PREVIOUS VALUES AS A UTF-8 STRING 
-                      CONCATENATED TOGETHER WITH COMMA SEPERATORS>}
-        """
-        sident_verify_msg = {'type':'sident_verify'}
+                      CONCATENATED TOGETHER WITH COMMA SEPERATORS>}"""
+        sident_verify_msg = {'type':'sident_verify',
+                             'timestamp':calendar.timegm(time.gmtime())}
         self._send_queue.put((sident_verify_msg, connection))
         return True
+
+    def request_server_address(self, connection):
+        """Request the best guess at the current server address from a client
+        peer. 
+
+        P2P nodes use the same JSON messaging style as the normal client and
+        server. address_request messages are of the form:
+
+        {'type':'address_request'
+         'timestamp':<UNIX TIMESTAMP>}
+
+        And a server_address message is of the form:
+
+        {'type':'server_address',
+         'key':<CRYPTOGRAPHIC KEY THAT UNIQUELY IDENTIFIES SERVER>,
+         'address':<SERVER ADDRESS>,
+         'port':<WHAT PORT THE SERVER LISTENS ON>,
+         'address_timestamp':<UNIX TIMESTAMP OF WHEN PEER RECEIVED ADDRESS>,
+         'signature':<VERIFICATION THAT INFORMATION CAME FROM SERVER ORIGINALLY>,
+         'timestamp':<UNIX TIMESTAMP OF WHEN MESSAGE WAS SENT>}"""
+        address_request = {'type':'sident_verify',
+                           'timestamp':calendar.timegm(time.gmtime())}
+        self._send_queue.put((address_request, connection))
+        return True
+        
 
     def send_loop(self):
         """Send loop that is meant to be started from a seperate thread of 
